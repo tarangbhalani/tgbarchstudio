36984791d36a27b527093957fe50af3f9839ca5f
Umang Bhalani <tarangbhalani53@gmail.com>
2021-11-10 19:20:49 +0530

    Major footer change, code minimalism and error debugging

--- main.js
+++ main.js
@@ -21,3 +21,24 @@ var preloader = document.getElementById('loading');
         document.getElementById("logo").style.opacity = '1';
       }
     })
+
+
+
+    mybutton = document.getElementById("back-to-top");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+    
+    function scrollFunction() {
+      if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        mybutton.style.display = "block";
+      } else {
+        mybutton.style.display = "none";
+      }
+    }
+    
+    // When the user clicks on the button, scroll to the top of the document
+    function topfunction() {
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
