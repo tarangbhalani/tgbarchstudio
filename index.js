b588469d32a6cd2c18080dab4c7fd4d9298606c5
Umang Bhalani <umang9427@gmail.com>
2021-11-22 18:50:00 +0530

    Reveal change, mobile view slider height decreased

--- index.js
+++ index.js
@@ -178,26 +178,15 @@ if (auto) {
 
 
 
+    /*-------------------------------- Reveal animation --------------------------------- */
 
-
-
-
-
-
-
-
-
-
-
-
-    /*---------------Reveal animation-------------------------------- */
     window.addEventListener('scroll', reveal);
     function reveal() {
       var reveals= document.querySelectorAll('.reveal');
       for (var i=0; i<reveals.length; i++){
         var windowheight = window.innerHeight;
         var revealtop = reveals[i].getBoundingClientRect().top;
-        var revealpoint = 30;
+        var revealpoint = 0;
         if(revealtop < windowheight - revealpoint){
           reveals[i].classList.add('activesection');
         }
